c843d90475b7993a3b560c2b13a2aab54c1d2ad4
Maximilian Hils <git@maximilianhils.com>
2011-09-03 00:24:31 +0200

    using beforeload now - working! :)

--- privacy/likeprivacy.js
+++ privacy/likeprivacy.js
@@ -1,11 +1,23 @@
-var privacyframes = document.getElementsByTagName("iframe");
+//var privacyframes = document.getElementsByTagName("iframe");
 var isFacebook = /.*facebook\.com\/plugins\/.*/i;
 var likeDummy = chrome.extension.getURL("like.html");
 
 //console.log([document,privacyframes,document.readyState]);
 
+document.addEventListener("beforeload", function(event) {
+  if(isFacebook.test(event.url))
+  {
+    console.log(["Caught resource load",event]); 
+	event.preventDefault();
+	event.target.src = likeDummy+ "#" + encodeURIComponent(event.url);
+  }
+  if(RegExp(likeDummy+".*","i").test(event.url))
+	console.log(["New resource load",event]); 
+}, true); 
+/*
 for(var i=0;i<privacyframes.length;i++)
 {
 	if(isFacebook.test(privacyframes[i].src))
 		privacyframes[i].src = likeDummy+ "#" + encodeURIComponent(privacyframes[i].src);
 }
+*/
