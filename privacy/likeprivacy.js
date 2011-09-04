56b2f2f4517d0124e7f0ee738660c900864332db
Maximilian Hils <git@maximilianhils.com>
2011-09-04 18:55:16 +0200

    Adding XFBML support

--- privacy/likeprivacy.js
+++ privacy/likeprivacy.js
@@ -1,8 +1,34 @@
-var isFacebookXFBML= /facebook.net\/.*all\.js/i;
+var isFacebook_XFBML= /(fbcdn|facebook).(net|com)\/.*all\.js/i;
 var isFacebook_Like = /facebook\.com\/plugins\//i;
 var likeDummy = chrome.extension.getURL("like.html");
 
 //console.log([document,privacyframes,document.readyState]);
+
+function fixXFBML(){
+	
+	var tags = ["fb:like"];
+	
+	var activatespan = document.createElement("span");
+	var activate = document.createElement("a");
+	activatespan.appendChild(activate);
+	activate.appendChild(document.createTextNode("Like"));
+	
+	activate.onclick = function(){localStorage.setItem("allowXFBML",true);};
+	
+	for(var i=0;i<tags.length;i++)
+	{
+		var elems = document.getElementsByTagName(tags[i]);
+		for(var j=0;j<elems.length;j++)
+		{
+			var elem = elems[j];
+			console.log([elem,activatespan]);
+			elem.parentNode.replaceChild(activatespan,elem);
+		}
+	}
+	
+}
+
+//if(window.fb)
 
 document.addEventListener("beforeload", function(event) {
 	if(isFacebook_Like.test(event.url))
@@ -11,10 +37,14 @@ document.addEventListener("beforeload", function(event) {
 		event.preventDefault();
 		event.target.src = likeDummy+ "#" + encodeURIComponent(event.url);
 	}
-	else if(isFacebook.test(event.url))
+	else if(isFacebook_XFBML.test(event.url))
 	{
-		//console.log(["Caught XFBML load",event]); 
+		if(localStorage.getItem("allowXFBML") == null)
+		{
+			event.preventDefault();
+			document.addEventListener( "DOMContentLoaded", fixXFBML, false );
+		}
 		
-		event.preventDefault();
+		
 	}
 }, true); 
