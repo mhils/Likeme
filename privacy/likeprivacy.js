7f9532cf916c171d9e96c1d04ddfb1c693be8fa3
Maximilian Hils <git@maximilianhils.com>
2011-09-04 20:04:25 +0200

    Solved styling issues

--- privacy/likeprivacy.js
+++ privacy/likeprivacy.js
@@ -4,20 +4,39 @@ var likeDummy = chrome.extension.getURL("like.html");
 
 //console.log([document,privacyframes,document.readyState]);
 
+function initXFBMLFake(){
+	var fileref=document.createElement("link")
+	fileref.setAttribute("rel", "stylesheet")
+	fileref.setAttribute("type", "text/css")
+	fileref.setAttribute("href", chrome.extension.getURL("like.css"));
+	document.head.appendChild(fileref);
+	console.log(chrome.extension.getURL("like.css"));
+}
+
 function fixXFBML(){
 	
 	var tags = ["fb:like"];
+	
+	var inited = false;
 	
 	var activatespan = document.createElement("span");
 	var activate = document.createElement("a");
 	activatespan.appendChild(activate);
 	activate.appendChild(document.createTextNode("Like"));
-	
-	activate.onclick = function(){localStorage.setItem("allowXFBML",true);};
-	
+	activate.className = "fakeLike";
+
+	activate.onclick = function(){
+		alert(1);
+		//localStorage.setItem("allowXFBML",true);
+	};
 	for(var i=0;i<tags.length;i++)
 	{
 		var elems = document.getElementsByTagName(tags[i]);
+		if(!inited && (elems.length > 0))
+		{
+			initXFBMLFake();
+			inited = true;
+		}
 		for(var j=0;j<elems.length;j++)
 		{
 			var elem = elems[j];
@@ -39,6 +58,7 @@ document.addEventListener("beforeload", function(event) {
 	}
 	else if(isFacebook_XFBML.test(event.url))
 	{
+		console.log(["Caught FB-XFBML load",event]); 
 		if(localStorage.getItem("allowXFBML") == null)
 		{
 			event.preventDefault();
